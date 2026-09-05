3e74ca261b357d7a10165b611df3b057c3e0bd41
mp <mohanapriyap2025ece@sece.ac.in>
2026-09-05 22:37:05 +0530

    add CheckifArraysSorted implementation to verify if an array is sorted

---
+++ Problem Solving/Array/CheckifArraysSorted.java
@@ -0,0 +1,17 @@
+class CheckifArraysSorted{
+    public static void main(String[] args){
+        int[] arr={1,2,3,4,5};
+        System.out.println(check(arr));
+    }
+    public static boolean check(int[] nums) {
+       int count=0;
+       for(int i=0;i<nums.length;i++){
+          if(nums[(i+1)%nums.length]<nums[i]){
+            count++;
+          }
+          if(count>1)
+             return false;
+       }
+       return true;
+    }
+}
